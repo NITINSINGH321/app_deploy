9388fee1b02a4f36ec55c4ad8cd67b2063bb474c
NITINSINGH321 <96173135+NITINSINGH321@users.noreply.github.com>
2024-09-06 01:34:13 +0530

    django_app.sh
    Updating link

--- django_app.sh
+++ django_app.sh
@@ -8,7 +8,7 @@ code_clone() {
     if [ -d "django-notes-app" ]; then
         echo "The code directory already exists. Skipping clone."
     else
-        git clone https://github.com/LondheShubham153/django-notes-app.git || {
+        git clone https://github.com/NITINSINGH321/django_app_deploy.sh.git || {
             echo "Failed to clone the code."
             return 1
         }
